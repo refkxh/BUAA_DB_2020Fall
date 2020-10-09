879b2b0096ba145d714717f8d9b8025d481d61ab
孔祥浩 <18373109@buaa.edu.cn>
2020-10-09 16:14:08 +0800

    modified schema

--- jiaowu/schema.sql
+++ jiaowu/schema.sql
@@ -5,22 +5,22 @@ create table student
 (
     sno    varchar(10) primary key,
     spwd   varchar(128) not null,
-    sname  varchar(32)  not null,
-    ssex   varchar(4)   not null check (ssex in ('男', '女')),
-    sid    varchar(18)  not null unique,
-    sgrade varchar(10),
-    sdept  varchar(32),
+    sname  nvarchar(16) not null,
+    ssex   nchar(1)     not null check (ssex in ('男', '女')),
+    sid    char(18)     not null unique,
+    sgrade nvarchar(10),
+    sdept  nvarchar(32),
     stel   varchar(11),
-    smail  varchar(30)
+    smail  varchar(32)
 );
 
 create table course
 (
     cno     int auto_increment primary key,
-    cname   varchar(32) not null,
-    ctype   varchar(10),
-    ccredit int         not null,
-    cdept   varchar(32),
-    ccap    int         not null,
-    cselect int         not null default '0'
+    cname   nvarchar(16) not null,
+    ctype   nvarchar(5),
+    ccredit int          not null,
+    cdept   nvarchar(16),
+    ccap    int          not null,
+    cselect int          not null default '0'
 );
