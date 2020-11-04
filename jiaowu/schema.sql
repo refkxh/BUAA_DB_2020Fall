c5850dd12e0da40a2d4c10bc68b64c0fbfd3d5d2
孔祥浩 <18373109@buaa.edu.cn>
2020-11-04 22:12:01 +0800

    added teacher schema

--- jiaowu/schema.sql
+++ jiaowu/schema.sql
@@ -9,8 +9,8 @@ create table student
     sname  varchar(32)  not null,
     ssex   char(2)      not null check (ssex in ('男', '女')),
     sid    char(18)     not null unique,
-    sgrade varchar(10),
-    sdept  varchar(32),
+    sgrade varchar(10) not null,
+    sdept  varchar(32) not null,
     stel   varchar(11),
     smail  varchar(32)
 );
@@ -24,6 +24,18 @@ create table course
     cdept   varchar(32),
     ccap    int         not null,
     cselect int         not null default '0'
+);
+
+create table teacher
+(
+    tno    varchar(10) primary key,
+    tpwd   varchar(128) not null,
+    tname  varchar(32)  not null,
+    tsex   char(2)      not null check (tsex in ('男', '女')),
+    ttitle varchar(16)  not null,
+    tdept  varchar(32) not null,
+    ttel   varchar(11),
+    tmail  varchar(32)
 );
 
 create table admin
@@ -41,6 +53,6 @@ create table student_course
     cno   int,
     score int,
     primary key (sno, cno),
-    foreign key (sno) references student(sno),
-    foreign key (cno) references course(cno)
+    foreign key (sno) references student (sno),
+    foreign key (cno) references course (cno)
 );
