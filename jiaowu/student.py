5e1bf1c620cee20ecb5c3802b197123db7340256
孔祥浩 <18373109@buaa.edu.cn>
2020-11-14 21:18:45 +0800

    try to fix teacher_course in student

--- jiaowu/student.py
+++ jiaowu/student.py
@@ -85,7 +85,9 @@ def list_unselected_courses():
     for course in courses:
         cursor.execute('select tname from teacher where tno in '
                        '(select tno from teacher_course where cno = %s)', (course['cno'],))
-        course_teachers[str(course['cno'])] = cursor.fetchall()['tname']
+        course_teachers[str(course['cno'])] = []
+        for name in cursor.fetchall():
+            course_teachers[str(course['cno'])].append(name['tname'])
     cursor.close()
     return render_template('student/list_unselected_courses.html', courses=courses, course_teachers=course_teachers)
 
@@ -125,7 +127,9 @@ def list_selected_courses():
     for course in courses:
         cursor.execute('select tname from teacher where tno in '
                        '(select tno from teacher_course where cno = %s)', (course['cno'],))
-        course_teachers[str(course['cno'])] = cursor.fetchall()['tname']
+        course_teachers[str(course['cno'])] = []
+        for name in cursor.fetchall():
+            course_teachers[str(course['cno'])].append(name['tname'])
     cursor.close()
     return render_template('student/list_selected_courses.html', courses=courses, course_teachers=course_teachers)
 
